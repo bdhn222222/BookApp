a33f1a894a77dfe3da9f8cea13e2b3498ddda231
bdhn222222 <nguyen25102003n@gmail.com>
2024-05-14 15:06:32 +0700

    140524 _ Part2

--- app/src/main/java/com/example/bookapp/SplashActivity.kt
+++ app/src/main/java/com/example/bookapp/SplashActivity.kt
@@ -1,11 +1,17 @@
 package com.example.bookapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+        },2000) //means 2 second
     }
 }
